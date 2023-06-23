62443246d9baea56b62050cdda3e60cec4ea70bc
Manuel Zuluaga <mazuluagaf@gmail.com>
2023-06-23 01:39:13 -0500

    App: create - Add: components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <h1>Desde Header Miñoco</h1>
+            <p>Hola Mundo</p>
+        </div>
+    )
+}
+
+export default Header; 
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+const ListadoPacientes = () => {
+  return (
+    <div>
+        <h1>Listado Pacientes</h1>
+    </div>
+  )
+}
+
+export default ListadoPacientes;
