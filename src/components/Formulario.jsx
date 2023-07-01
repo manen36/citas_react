d4c86671f6208bafd7618d28252e384a81c62a13
Manuel Zuluaga <mazuluagaf@gmail.com>
2023-07-01 18:48:41 -0500

    Add: funcion Eliminar Paciente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Error from "./Error";
 
-const Formulario = ({pacientes, setPacientes, paciente}) => {
+const Formulario = ({pacientes, setPacientes, paciente, setPaciente}) => {
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
   const [email, setEmail] = useState('');
@@ -45,12 +45,24 @@ const Formulario = ({pacientes, setPacientes, paciente}) => {
         propietario, 
         email, 
         fecha, 
-        sintomas,
-        id: generarId()
+        sintomas        
       }
-      // console.log(objetoPaciente)
 
-      setPacientes([...pacientes, objetoPaciente]);
+      if(paciente.id) {
+        // Editando Registro
+        objetoPaciente.id = paciente.id
+
+        const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState )
+
+        setPacientes(pacientesActualizados)
+        setPaciente({})
+      
+      } else {
+        // Nuevo Registro
+        objetoPaciente.id = generarId();
+        setPacientes([...pacientes, objetoPaciente]);
+
+      }
 
       //Reiniciar Formluario
       setNombre('')
@@ -137,7 +149,7 @@ const Formulario = ({pacientes, setPacientes, paciente}) => {
 
             <input type="submit" 
                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-                   value="Agregar Paciente" 
+                   value={ paciente.id ? 'Editar Paciente' : 'Agregar Paciente' } 
             />
         </form>
     </div>
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
   
    
   
@@ -21,6 +21,7 @@ const ListadoPacientes = ({pacientes, setPaciente}) => {
                         key={paciente.id}
                         paciente={paciente}
                         setPaciente={setPaciente}
+                        eliminarPaciente={eliminarPaciente}
                     />
                 ))}
             </>
